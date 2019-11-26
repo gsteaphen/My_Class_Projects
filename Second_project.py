4500d53de0e9435e723f528abf0f6fd0c1838e8d
gsteaphen <58047393+gsteaphen@users.noreply.github.com>
2019-11-25 21:48:44 -0600

    Create Second_project.py

---
+++ Second_project.py
@@ -0,0 +1,8 @@
+def poundsToMetricFunction(kilograms, grams):
+pounds = float(input("enter the amount of pounds:  ")
+               kilograms = pounds * 2.2
+               grams = kilograms * 1000
+
+               print('The amount of pounds you entered is ', pounds,
+                     ' This is ', kilograms, ' kilograms ', 'and', grams,
+                     'grams' )
